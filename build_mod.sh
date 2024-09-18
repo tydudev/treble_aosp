338cba3e49bf6f1504e6be8ac50720371bb1ba3a
tydu <salvinoschillaci@gmail.com>
2024-09-18 21:08:15 +0200

    Update build_mod.sh

--- build_mod.sh
+++ build_mod.sh
@@ -70,8 +70,19 @@ buildVariant() {
     lunch "$1"-ap2a-userdebug
     make -j$(nproc --all) installclean
     make -j$(nproc --all) systemimage
-    make -j$(nproc --all) target-files-package otatools
-    bash $BL/sign.sh "vendor/ponces-priv/keys" $OUT/signed-target_files.zip
+    #make -j$(nproc --all) target-files-package otatools
+    #bash $BL/sign.sh "vendor/ponces-priv/keys" $OUT/signed-target_files.zip
+    echo "--> Building $1"
+    lunch "$1"-ap2a-userdebug
+    make -j$(nproc --all) installclean
+    make -j$(nproc --all) systemimage
+    #make -j$(nproc --all) target-files-package otatools
+    #bash $BL/sign.sh "vendor/ponces-priv/keys" $OUT/signed-target_files.zip
+    subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=salvinoschillaci@gmail.com'
+    mkdir ~/.android-certs
+    for x in releasekey platform shared media networkstack; do ./development/tools/make_key ~/.android-certs/$x "$subject"; done
+    make dist
+    sign_target_files_apks -o --default_key_mappings ~/.android-certs out/dist/*-target_files-*.zip signed-target_files.zip
     unzip -jo $OUT/signed-target_files.zip IMAGES/system.img -d $OUT
     mv $OUT/system.img $BD/system-"$1".img
     echo
