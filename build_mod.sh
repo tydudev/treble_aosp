be9f895eaad3cfa08aecaf5235ba54863afd53ac
tydu <salvinoschillaci@gmail.com>
2024-10-09 06:20:26 +0200

    Update build_mod.sh

--- build_mod.sh
+++ build_mod.sh
@@ -13,6 +13,7 @@ set -e
 BL=$PWD/treble_aosp
 BD=$HOME/builds
 BV=$1
+Sign=true
 
 initRepos() {
     echo "--> Initializing workspace"
@@ -33,20 +34,8 @@ syncRepos() {
 }
 
 applyPatches() {
-    echo "--> Applying TrebleDroid patches"
-    bash $BL/patch.sh $BL trebledroid
-    echo
-
-    echo "--> Applying personal patches"
-    bash $BL/patch.sh $BL personal
-    echo
-
-    echo "--> Generating makefiles"
-    cd device/phh/treble
-    cp $BL/build/aosp.mk .
-    bash generate.sh aosp
-    cd ../../..
-    echo
+    echo "Applying patch group ${1}"
+    bash ./lineage_build_unified/apply_patches.sh ./lineage_patches_unified/${1}
 }
 
 setupEnv() {
@@ -68,13 +57,15 @@ buildTrebleApp() {
 buildVariant() {
    echo "--> Building $1"
     lunch "$1"-ap2a-userdebug
-    make -j$(nproc --all) installclean
-    make -j$(nproc --all) systemimage
-    make -j$(nproc --all) target-files-package otatools
+    make -j6 installclean
+    make -j6 systemimage
+    make -j6 target-files-package otatools
     #bash $BL/sign.sh "vendor/ponces-priv/keys" $OUT/signed-target_files.zip
-    #subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=salvinoschillaci@gmail.com'
-    #mkdir ~/.android-certs
-    #for x in releasekey sdk_sandbox platform shared media networkstack; do ./development/tools/make_key ~/.android-certs/$x "$subject"; done
+    if [ "$Sign" = true ]; then
+    subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=salvinoschillaci@gmail.com'
+    mkdir ~/.android-certs
+    for x in releasekey sdk_sandbox platform shared media networkstack; do ./development/tools/make_key ~/.android-certs/$x "$subject"; done
+    fi
     #make dist
     sign_target_files_apks -o --default_key_mappings ~/.android-certs out/target/product/tdgsi_arm64_ab/obj/PACKAGING/target_files_intermediates/*-target_files*.zip signed-target_files.zip
     unzip -jo signed-target_files.zip IMAGES/system.img -d $BD
@@ -102,44 +93,6 @@ buildVariants() {
     #buildVndkliteVariant treble_a64_bgN
     #buildVndkliteVariant treble_arm64_bvN
     #buildVndkliteVariant treble_arm64_bgN
-}
-
-generatePackages() {
-    echo "--> Generating packages"
-    buildDate="$(date +%Y%m%d)"
-    find $BD/ -name "system-treble_*.img" | while read file; do
-        filename="$(basename $file)"
-        [[ "$filename" == *"_a64"* ]] && arch="arm32_binder64" || arch="arm64"
-        [[ "$filename" == *"_bvN"* ]] && variant="vanilla" || variant="gapps"
-        [[ "$filename" == *"-vndklite"* ]] && vndk="-vndklite" || vndk=""
-        name="aosp-${arch}-ab-${variant}${vndk}-14.0-$buildDate"
-        xz -cv "$file" -T0 > $BD/"$name".img.xz
-    done
-    rm -rf $BD/system-*.img
-    echo
-}
-
-generateOta() {
-    echo "--> Generating OTA file"
-    version="$(date +v%Y.%m.%d)"
-    buildDate="$(date +%Y%m%d)"
-    timestamp="$START"
-    json="{\"version\": \"$version\",\"date\": \"$timestamp\",\"variants\": ["
-    find $BD/ -name "aosp-*-14.0-$buildDate.img.xz" | sort | {
-        while read file; do
-            filename="$(basename $file)"
-            [[ "$filename" == *"-arm32"* ]] && arch="a64" || arch="arm64"
-            [[ "$filename" == *"-vanilla"* ]] && variant="v" || variant="g"
-            [[ "$filename" == *"-vndklite"* ]] && vndk="-vndklite" || vndk=""
-            name="treble_${arch}_b${variant}N${vndk}"
-            size=$(wc -c $file | awk '{print $1}')
-            url="https://github.com/ponces/treble_aosp/releases/download/$version/$filename"
-            json="${json} {\"name\": \"$name\",\"size\": \"$size\",\"url\": \"$url\"},"
-        done
-        json="${json%?}]}"
-        echo "$json" | jq . > $BL/config/ota.json
-    }
-    echo
 }
 
 START=$(date +%s)
